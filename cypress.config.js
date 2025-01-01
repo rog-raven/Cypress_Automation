fce31a6febf0c5e190a62fcc5920bc72d263ce37
Ajith Palani <ajith.palani.-nd@disney.com>
2025-01-01 18:25:23 +0530

    forex

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     supportFile: "cypress/support/e2e.js", // Update path if needed
-    baseUrl: "https://adactinhotelapp.com/", // Replace with your application's base URL
+  //  baseUrl: "https://adactinhotelapp.com/", // Replace with your application's base URL
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
